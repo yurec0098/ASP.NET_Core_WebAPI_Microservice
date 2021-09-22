298127d2524b52ee18c3e848f83820fa65bdae95
yurec0098 <yurec0098@rambler.ru>
2021-09-22 14:52:49 +0300

    Lesson_5 Homework

--- Lesson_5/MetricsAgent.Services/Jobs/CpuMetricJob.cs
+++ Lesson_5/MetricsAgent.Services/Jobs/CpuMetricJob.cs
@@ -3,6 +3,7 @@ using MetricsAgent.DB.Entity;
 using Quartz;
 using System;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace MetricsAgent.Jobs
@@ -16,11 +17,11 @@ namespace MetricsAgent.Jobs
 		private readonly IDbRepository<NetworkMetric> _networkRepo;
 
 		// счетчик для метрики CPU
-		private PerformanceCounter _cpuCounter;
-		private PerformanceCounter _ramCounter;
-		private PerformanceCounter _hddCounter;
-		private PerformanceCounter _networkCounter;
-		private PerformanceCounter _dotnetCounter;
+		private PerformanceCounter _cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+		private PerformanceCounter _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+		private PerformanceCounter _hddCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+		private PerformanceCounter _dotnetCounter = new PerformanceCounter(".NET CLR Memory", "# Bytes in all Heaps", "_Global_");
+		private PerformanceCounter _networkCounter = new PerformanceCounter("Network Adapter", "Bytes Total/sec");
 
 		public MetricsJob(
 			IDbRepository<CpuMetric> cpuRepo,
@@ -35,12 +36,9 @@ namespace MetricsAgent.Jobs
 			_dotnetRepo = dotnetRepo;
 			_networkRepo = networkRepo;
 
-			_cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-			_ramCounter = new PerformanceCounter("Memory", "Available MBytes");
-			_hddCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
-			//_networkCounter = new PerformanceCounter("Network Adapter", "Bytes Total/sec", "intel[r] centrino[r] advanced-n 6200 agn");
-			_networkCounter = new PerformanceCounter("Network Adapter", "Bytes Total/sec", "_Total");
-			_dotnetCounter = new PerformanceCounter(".NET CLR Memory", "# Bytes in all Heaps", "_Global");
+			foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+				if (ni.OperationalStatus == OperationalStatus.Up && ni.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+					_networkCounter.InstanceName = ni.Description;
 		}
 
 		public Task Execute(IJobExecutionContext context)
@@ -51,17 +49,17 @@ namespace MetricsAgent.Jobs
 			var cpuValue = Convert.ToInt32(_cpuCounter.NextValue());
 			var ramValue = Convert.ToInt32(_ramCounter.NextValue());
 			var hddValue = Convert.ToInt32(_hddCounter.NextValue());
-			var networkValue = Convert.ToInt32(_networkCounter.NextValue());
 			var dotnetValue = Convert.ToInt32(_dotnetCounter.NextValue());
+			var networkValue = Convert.ToInt32(_networkCounter.NextValue());
 
 			// теперь можно записать полученные данные при помощи репозиториев
 			_cpuRepo.AddAsync(new CpuMetric { Time = time, Value = cpuValue });
 			_ramRepo.AddAsync(new RamMetric { Time = time, Value = ramValue });
 			_hddRepo.AddAsync(new HddMetric { Time = time, Value = hddValue });
-			_dotnetRepo.AddAsync(new DotNetMetric { Time = time, Value = networkValue });
-			_networkRepo.AddAsync(new NetworkMetric { Time = time, Value = dotnetValue });
+			_dotnetRepo.AddAsync(new DotNetMetric { Time = time, Value = dotnetValue });
+			_networkRepo.AddAsync(new NetworkMetric { Time = time, Value = networkValue });
 
-			//Debug.WriteLine($"CpuMetricJob, {DateTime.Now: ddd HH:mm:ss} cpuPercents: {cpuUsageInPercents}% RAM: {ramCounter}MB");
+			//Debug.WriteLine($"CpuMetricJob, {DateTime.Now: ddd HH:mm:ss} cpuPercents: {cpuValue}% RAM: {ramValue}MB Network: {networkValue}");
 			return Task.CompletedTask;
 		}
 	}
