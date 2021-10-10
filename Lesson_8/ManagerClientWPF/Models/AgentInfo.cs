3c3d58db205cb80729b8889a4784a3b4c042c47b
yurec0098 <yurec0098@rambler.ru>
2021-10-11 01:21:21 +0300

    Homework 8

--- Lesson_8/ManagerClientWPF/Models/AgentInfo.cs
+++ Lesson_8/ManagerClientWPF/Models/AgentInfo.cs
@@ -9,12 +9,4 @@ namespace ManagerClientWPF.Models
 		public string AgentName { get; set; }
 		public bool IsEnabled { get; set; }
 	}
-	public class AgentMetricEntity
-	{
-		public long Id { get; set; }
-		public double Value { get; set; }
-		//public DateTime Time { get; set; }
-		public long Time { get; set; }
-		//public long? AgentId { get; set; }
-	}
 }
---
+++ Lesson_8/ManagerClientWPF/Models/AgentMetricEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ManagerClientWPF.Models
+{
+	public class AgentMetricEntity
+	{
+		public long Id { get; set; }
+		public double Value { get; set; }
+		//public DateTime Time { get; set; }
+		public long Time { get; set; }
+		//public long? AgentId { get; set; }
+	}
+}
